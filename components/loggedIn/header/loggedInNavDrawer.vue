ee70c0f5ec355618ae5bc7b33a4ce6b862633663
ishida_masakazu <u225210b@gmail.com>
2021-03-20 16:17:26 +0900

    add_navigation_drawer

---
+++ components/loggedIn/header/loggedInNavDrawer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-navigation-drawer
+    v-model="setDrawer"
+    app
+    clipped
+    mobile-breakpoint="960"
+  >
+    <v-list>
+      <v-divider />
+
+      <v-list-item
+        v-for="(nav, i) in navMenus"
+        :key="`nav-${i}`"
+        :to="$my.projectLinkTo($route.params.id, nav.name)"
+      >
+        <v-list-item-icon>
+          <v-icon v-text="nav.icon" />
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ $my.pageTitle(nav.name) }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        navMenus: [
+          { name: 'project-id-dashboard', icon: 'mdi-view-dashboard' },
+          { name: 'project-id-layouts', icon: 'mdi-view-compact' },
+          { name: 'project-id-pages', icon: 'mdi-image' },
+          { name: 'project-id-components', icon: 'mdi-view-comfy' },
+          { name: 'project-id-settings', icon: 'mdi-cog' },
+          { name: 'project-id-help', icon: 'mdi-help-circle' }
+        ]
+      }
+    },
+    props: {
+      drawer: {
+        type: Boolean,
+        default: null
+      }
+    },
+    computed: {
+      setDrawer: {
+        get () { return this.drawer },
+        set (val) { return this.$emit('update:drawer', val) }
+      }
+    }
+  }
+</script>
